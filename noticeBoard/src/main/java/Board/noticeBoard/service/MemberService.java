63d0b52fc11958ba623d5ff998970d741d9fc6ce
dddgg9511 <39615281+dddgg9511@users.noreply.github.com>
2021-02-23 21:17:19 +0900

    service class

--- noticeBoard/src/main/java/Board/noticeBoard/service/MemberService.java
+++ noticeBoard/src/main/java/Board/noticeBoard/service/MemberService.java
@@ -1,7 +1,11 @@
 package Board.noticeBoard.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.google.common.base.FinalizablePhantomReference;
 
 import Board.noticeBoard.dao.MemberDAO;
 import Board.noticeBoard.domain.Member;
@@ -11,7 +15,23 @@ public class MemberService {
 	@Autowired
 	MemberDAO memberDAO;
 	
-	public void signUpMember(Member member) throws Exception{
+	public void signUpMember(Member member) throws Exception {
+		Optional<Member> findMember = memberDAO.findById(member.getId());
+		findMember.ifPresent(mem -> {
+			throw new RuntimeException();
+		});
 		memberDAO.save(member);
 	}
+	public void signIn(String id, String password) {
+		
+	}
+	
+	public void Leave(Member member) {
+		
+	}
+	
+	public void update(Member member) {
+		
+	}
+	
 }
